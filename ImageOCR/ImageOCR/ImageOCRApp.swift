6efecf438e6f24a1221bcb1b94c6ef86d75fdacc
Krzysztof Lech <krzysztof.lech@twoupdigital.com>
2023-01-06 18:49:46 +0100

    Xcode init project

---
+++ ImageOCR/ImageOCR/ImageOCRApp.swift
@@ -0,0 +1,13 @@
+//  ImageOCRApp.swift
+//  Created by Krzysztof Lech on 06/01/2023.
+
+import SwiftUI
+
+@main
+struct ImageOCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ ImageOCR/ImageOCR/MainView.swift
@@ -0,0 +1,22 @@
+//  ContentView.swift
+//  Created by Krzysztof Lech on 06/01/2023.
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
